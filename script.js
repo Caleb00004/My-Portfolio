bc962fff1ce5ec76360aebe44ad9845fcd062ed1
caleb <calebakpan7@gmail.com>
2022-12-21 23:46:28 +0100

    made modifications

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log('here')
+// console.log('here')
 
-let currentURL = window.location.hash;
+// let currentURL = window.location.hash;
 
-document.getElementById('nav-item').onclick = function() {
-    console.log('clicked')
-}
-if (currentURL == '#about') {
-//    console.log(document.getElementById('nav-item').className += 'active')
-    document.getElementById('nav-item').className += 'active'
-} else {
-    console.log(window.location)
-}
+// document.getElementById('nav-item').onclick = function() {
+//     console.log('clicked')
+// }
+// if (currentURL == '#about') {
+// //    console.log(document.getElementById('nav-item').className += 'active')
+//     document.getElementById('nav-item').className += 'active'
+// } else {
+//     console.log(window.location)
+// }
