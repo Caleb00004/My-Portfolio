681fce7c15f80d0c5c6947150802fef8ce2ce93d
caleb <calebakpan7@gmail.com>
2023-06-25 18:16:17 +0100

    added mobile projects

--- script.js
+++ script.js
@@ -1,13 +1,152 @@
-// console.log('here')
+const WebProjects = `
+<div class="project-item">
+                    <img src="./images/theNetwork.png">
 
-// let currentURL = window.location.hash;
+                    <div class="project-details">
+                        <h2>theNetwork</h2>
+                        <p>A MERN Stack Social media web application</p>
 
-// document.getElementById('nav-item').onclick = function() {
-//     console.log('clicked')
-// }
-// if (currentURL == '#about') {
-// //    console.log(document.getElementById('nav-item').className += 'active')
-//     document.getElementById('nav-item').className += 'active'
-// } else {
-//     console.log(window.location)
-// }
+                        <div class="project-icons">
+                            <a href="https://the-network-bice.vercel.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/theNetwork" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="project-item">
+                    <img src="./images/crypto-tracker.png">
+
+                    <div class="project-details">
+                        <h2>Crypto Tracker</h2>
+                        <p>A web applicaton built with react that tracks live data of crypto currencies</p>
+
+                        <div class="project-icons">
+                            <a href="https://crypto-tracker-rosy.vercel.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/crypto-tracker" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="project-item">
+                    <img src="./images/quizzical.png">
+
+                    <div class="project-details">
+                        <h2>quizzical</h2>
+                        <p>A quiz Application built with react and Opentdb API</p>
+
+                        <div class="project-icons">
+                            <a href="https://ornate-tulumba-3e1234.netlify.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/Quizzical-App" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="project-item">
+                    <img src="./images/game-boyy.png">
+
+                    <div class="project-details">
+                        <h2>Game Boyy</h2>
+                        <p>A fun Javascript apllication that consist of different javascript game projects i've worked on</p>
+
+                        <div class="project-icons">
+                            <a href="https://resplendent-panda-e234c4.netlify.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/Game-Boyy" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="project-item">
+                    <img src="./images/cjc-markets.png">
+
+                    <div class="project-details">
+                        <h2>CJC Markets</h2>
+                        <p>Clone of a companies website. (CJC Markets). Built with HTML & CSS</p>
+
+                        <div class="project-icons">
+                            <a href="https://loquacious-dragon-8ea6ab.netlify.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/CJC-Markets" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="project-item">
+                    <img src="./images/book-management.png">
+
+                    <div class="project-details">
+                        <h2>Book Management</h2>
+                        <p>A CRUD Application built with React, Redux RTK and expressJS.</p>
+
+                        <div class="project-icons">
+                            <a href="https://book-management-rtei.vercel.app/"target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/Book-Management" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+                </div>
+
+`
+
+const MobileProjects = `
+    <div class="project-item">
+        <img src="./images/shoe_store.jpg" style="height: 25em;   object-fit: contain;">
+
+        <div class="project-details">
+            <h2>Shoe Store<span class="andriod_tag">(Andriod)</span></h2>
+            <p> Shoe Store is a stylish and user-friendly mobile application built with React Native. Browse through a wide selection of trendy shoes on the home screen,
+                each showcased with captivating images and essential details. Dive into the Shoe screen to explore individual shoe models in more detail, with descriptions</p>
+
+            <div class="project-icons">
+                <a href="https://expo.dev/@caleb112/shoe-store" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20 3H7c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h6c1.103 0 2-.897 2-2h8c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM9.997 19H4V9h6l-.003 10zm10-2H12V9c0-1.103-.897-2-2-2H7V5h13l-.003 12z"></path></svg><span>App Link</span></a>
+                <a href="https://github.com/Caleb00004/shoe_store_mobile" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+            </div>
+        </div>
+        </div>
+
+        <div class="project-item">
+        <img src="./images/food_order.jpg" style="height: 25em;   object-fit: contain;">
+
+        <div class="project-details">
+            <h2>Sweets App<span class="andriod_tag">(Andriod)</span></h2>
+            <p> Sweets App is a delightful mobile application built with React Native, dedicated to satisfying your sweet tooth.
+                Indulge in a wide array of mouthwatering sweets and treats showcased on the home screen, featuring enticing images and enticing descriptions. </p>
+
+            <div class="project-icons">
+                <a href="https://expo.dev/@caleb112/orderingApp"target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20 3H7c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h6c1.103 0 2-.897 2-2h8c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM9.997 19H4V9h6l-.003 10zm10-2H12V9c0-1.103-.897-2-2-2H7V5h13l-.003 12z"></path></svg><span>App Link</span></a>
+                <a href="https://github.com/Caleb00004/FoodApp-Mobile-" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+            </div>
+        </div>
+    </div>
+`
+
+const ProjectsContainer = document.getElementById("switch_section")
+const web = document.getElementById('web')
+const mobile = document.getElementById('mobile')
+
+ProjectsContainer.innerHTML = WebProjects
+
+web.style.color = 'rgb(248, 160, 7)'
+web.style.borderBottomColor = 'rgb(248, 160, 7)'
+// web.style.borderBottom = '2px solid '
+
+web.addEventListener('click', () => {
+    ProjectsContainer.innerHTML = WebProjects
+    web.style.color = 'rgb(248, 160, 7)'
+    web.style.borderBottomColor = 'rgb(248, 160, 7)'
+    mobile.style.color = 'unset'
+    mobile.style.borderBottomColor = 'aliceblue'
+})
+
+mobile.addEventListener('click', () => {
+    ProjectsContainer.innerHTML = MobileProjects
+    mobile.style.color = 'rgb(248, 160, 7)'
+    mobile.style.borderBottomColor = 'rgb(248, 160, 7)'
+    web.style.color = 'unset'
+    web.style.borderBottomColor = 'aliceblue'
+})
+    
+
+// console.log(section)
+// section.innerHTML(`
+//     <div>
+//         <h1>Hello Boys</h1>
+//     </div>
+// `)
