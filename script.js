9bf0e2ea1be515bc2f273b6fa11fa85c5ca80d40
caleb <calebakpan7@gmail.com>
2024-07-27 10:03:02 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,215 @@
+const WebProjects = `
+                <div class="project-item">
+                    <img src="./images/unikrib.png">
+
+                    <div class="project-details">
+                        <h2>UNIKRIB</h2>
+                        <p>A startup aimed to help university students find Apartments and also get access to local service providers and vendors </p>
+
+                        <div class="project-icons">
+                            <a href="https://www.unikribafrica.com/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="project-item">
+                    <img src="./images/theNetwork.png">
+
+                    <div class="project-details">
+                        <h2>theNetwork</h2>
+                        <p>A MERN Stack Social media web application</p>
+
+                        <div class="project-icons">
+                            <a href="https://the-network-bice.vercel.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/theNetwork" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="project-item">
+                    <img src="./images/crypto-tracker.png">
+
+                    <div class="project-details">
+                        <h2>Crypto Tracker</h2>
+                        <p>A web applicaton built with react that tracks live data of crypto currencies</p>
+
+                        <div class="project-icons">
+                            <a href="https://crypto-tracker-rosy.vercel.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/crypto-tracker" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="project-item">
+                    <img src="./images/quizzical.png">
+
+                    <div class="project-details">
+                        <h2>quizzical</h2>
+                        <p>A quiz Application built with react and Opentdb API</p>
+
+                        <div class="project-icons">
+                            <a href="https://ornate-tulumba-3e1234.netlify.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/Quizzical-App" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="project-item">
+                    <img src="./images/game-boyy.png">
+
+                    <div class="project-details">
+                        <h2>Game Boyy</h2>
+                        <p>A fun Javascript apllication that consist of different javascript game projects i've worked on</p>
+
+                        <div class="project-icons">
+                            <a href="https://resplendent-panda-e234c4.netlify.app/" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/Game-Boyy" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="project-item">
+                    <img src="./images/book-management.png">
+
+                    <div class="project-details">
+                        <h2>Book Management</h2>
+                        <p>A CRUD Application built with React, Redux RTK and expressJS.</p>
+
+                        <div class="project-icons">
+                            <a href="https://book-management-rtei.vercel.app/"target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="M20 17.722c.595-.347 1-.985 1-1.722V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v11c0 .736.405 1.375 1 1.722V18H2v2h20v-2h-2v-.278zM5 16V5h14l.002 11H5z"></path></svg><span>live Site</span></a>
+                            <a href="https://github.com/Caleb00004/Book-Management" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                        </div>
+                    </div>
+                </div>
+
+`
+
+const MobileProjects = `
+        <div class="project-item">
+            <img src="./images/machicare.png" style="height: 25em;   object-fit: contain;">
+
+            <div class="project-details">
+                <h2>MachiCare</h2>
+                <p> A Telemedicine app aimed at seamlessly connecting users to healthcare services and streamlining waiting time.</p>
+
+                <div class="project-icons">
+                    <a href="https://play.google.com/store/apps/details?id=com.machikunzult.machicare" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(24, 229, 68, 1);transform: ;msFilter:;"><path d="M18.344 8.71c-.707 0-1.279.572-1.279 1.278v4.53a1.278 1.278 0 1 0 2.558 0v-4.53c0-.706-.572-1.278-1.279-1.278zm-12.688 0c-.707 0-1.279.572-1.279 1.278v4.53a1.278 1.278 0 1 0 2.558 0v-4.53A1.277 1.277 0 0 0 5.656 8.71zm8.889-3.809.768-1.388a.343.343 0 0 0-.133-.47.344.344 0 0 0-.471.135l-.789 1.42c-.588-.235-1.236-.368-1.92-.368s-1.332.132-1.92.367l-.789-1.418a.344.344 0 0 0-.471-.135.345.345 0 0 0-.133.47l.768 1.388C8.072 5.69 7.148 7.099 7.148 8.71c0 .011 0 .021.002.034-.002.006-.002.013-.002.019v.001h9.703V8.71c.001-1.611-.923-3.019-2.306-3.809zM9.76 6.869a.346.346 0 1 1 .005-.693.346.346 0 0 1-.005.693zm4.48 0a.347.347 0 0 1 0-.692.346.346 0 0 1 0 .692zM7.148 16.12c0 .646.525 1.171 1.174 1.171h.586v2.401a1.279 1.279 0 0 0 2.558 0v-2.401h1.066v2.401c0 .707.572 1.278 1.277 1.278.709 0 1.281-.571 1.281-1.278v-2.401h.586c.648 0 1.174-.524 1.174-1.171V9.028H7.148v7.092z"></path></svg><span>Andriod</span></a>
+                    <a href="https://apps.apple.com/de/app/machicare/id6463551232" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M19.665 16.811a10.316 10.316 0 0 1-1.021 1.837c-.537.767-.978 1.297-1.316 1.592-.525.482-1.089.73-1.692.744-.432 0-.954-.123-1.562-.373-.61-.249-1.17-.371-1.683-.371-.537 0-1.113.122-1.73.371-.616.25-1.114.381-1.495.393-.577.025-1.154-.229-1.729-.764-.367-.32-.826-.87-1.377-1.648-.59-.829-1.075-1.794-1.455-2.891-.407-1.187-.611-2.335-.611-3.447 0-1.273.275-2.372.826-3.292a4.857 4.857 0 0 1 1.73-1.751 4.65 4.65 0 0 1 2.34-.662c.46 0 1.063.142 1.81.422s1.227.422 1.436.422c.158 0 .689-.167 1.593-.498.853-.307 1.573-.434 2.163-.384 1.6.129 2.801.759 3.6 1.895-1.43.867-2.137 2.08-2.123 3.637.012 1.213.453 2.222 1.317 3.023a4.33 4.33 0 0 0 1.315.863c-.106.307-.218.6-.336.882zM15.998 2.38c0 .95-.348 1.838-1.039 2.659-.836.976-1.846 1.541-2.941 1.452a2.955 2.955 0 0 1-.021-.36c0-.913.396-1.889 1.103-2.688.352-.404.8-.741 1.343-1.009.542-.264 1.054-.41 1.536-.435.013.128.019.255.019.381z"></path></svg><span>Apple</span></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-item">
+            <img src="./images/shoe_store.jpg" style="height: 25em;   object-fit: contain;">
+
+            <div class="project-details">
+                <h2>Shoe Store<span class="andriod_tag">(Andriod)</span></h2>
+                <p> Shoe Store is a stylish and user-friendly mobile application built with React Native. Browse through a wide selection of trendy shoes on the home screen,
+                    each showcased with captivating images and essential details. Dive into the Shoe screen to explore individual shoe models in more detail, with descriptions</p>
+
+                <div class="project-icons">
+                    <a href="https://expo.dev/@caleb112/shoe-store" target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;transform: ;msFilter:;"><path d="M20 3H7c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h6c1.103 0 2-.897 2-2h8c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM9.997 19H4V9h6l-.003 10zm10-2H12V9c0-1.103-.897-2-2-2H7V5h13l-.003 12z"></path></svg><span>App Link</span></a>
+                    <a href="https://github.com/Caleb00004/shoe_store_mobile" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-item">
+            <img src="./images/food_order.jpg" style="height: 25em;   object-fit: contain;">
+
+            <div class="project-details">
+                <h2>Sweets App<span class="andriod_tag">(Andriod)</span></h2>
+                <p> Sweets App is a delightful mobile application built with React Native, dedicated to satisfying your sweet tooth.
+                    Indulge in a wide array of mouthwatering sweets and treats showcased on the home screen, featuring enticing images and enticing descriptions. </p>
+
+                <div class="project-icons">
+                    <a href="https://expo.dev/@caleb112/orderingApp"target="_blank" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;transform: ;msFilter:;"><path d="M20 3H7c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h6c1.103 0 2-.897 2-2h8c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM9.997 19H4V9h6l-.003 10zm10-2H12V9c0-1.103-.897-2-2-2H7V5h13l-.003 12z"></path></svg><span>App Link</span></a>
+                    <a href="https://github.com/Caleb00004/FoodApp-Mobile-" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #ffffff;"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg><span>code</span></a>
+                </div>
+            </div>
+        </div>
+`
+
+// FOR NAVBAR
+document.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > (window.innerHeight - 100)) {
+        // navbar.style.display = 'block';
+        navbar.style.opacity = 1;
+        navbar.style.pointerEvents = "all";
+    } else {
+        navbar.style.opacity = 0;
+        navbar.style.pointerEvents = "none";
+    }
+});
+
+// ON SCROLL ANIMATIONS
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+
+// Web and Mobile Projects Section
+const ProjectsContainer = document.getElementById("switch_section")
+const web = document.getElementById('web')
+const mobile = document.getElementById('mobile')
+
+ProjectsContainer.innerHTML = WebProjects
+
+web.style.color = 'rgb(248, 160, 7)'
+web.style.borderBottomColor = 'rgb(248, 160, 7)'
+// web.style.borderBottom = '2px solid '
+
+web.addEventListener('click', () => {
+    ProjectsContainer.innerHTML = WebProjects
+    web.style.color = 'rgb(248, 160, 7)'
+    web.style.borderBottomColor = 'rgb(248, 160, 7)'
+    mobile.style.color = 'unset'
+    mobile.style.borderBottomColor = 'aliceblue'
+})
+
+mobile.addEventListener('click', () => {
+    ProjectsContainer.innerHTML = MobileProjects
+    mobile.style.color = 'rgb(248, 160, 7)'
+    mobile.style.borderBottomColor = 'rgb(248, 160, 7)'
+    web.style.color = 'unset'
+    web.style.borderBottomColor = 'aliceblue'
+})
+
+// NAV MOBILE
+const openNav = document.getElementById('open_nav')
+const closeNav = document.getElementById('close_nav')
+const menuIconsContainer = document.getElementById("menu_icons_container")
+var links = document.querySelectorAll(".phone_nav_contents a");
+// const openNav = document.querySelectorAll('.open_icon')
+const phoneNav = document.getElementById('phone_nav')
+
+const openIcon = "<img src='./assests/menu.svg' id='open_nav' class='open_icon' />"
+const closeIcon = "<img src='./assests/close.svg' id='open_nav' class='open_icon' />"
+
+links.forEach(function (link) {
+    link.addEventListener("click", function () {
+        openNav.style.display = 'flex'
+        closeNav.style.display = 'none'
+        phoneNav.style.transform = 'translateY(-120%)'
+    });
+});
+
+closeNav.addEventListener('click', function () {
+    console.log('close clicked')
+    openNav.style.display = 'flex'
+    closeNav.style.display = 'none'
+    phoneNav.style.transform = 'translateY(-120%)'
+})
+
+openNav.addEventListener('click', function () {
+    phoneNav.style.transform = 'translateY(0%)'
+    closeNav.style.display = 'flex'
+    openNav.style.display = 'none'
+})
